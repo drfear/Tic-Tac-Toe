1e4ea928390d2bda72b0512e5a02f9617245c854
Chris Feher <chris.feher@shopify.com>
2020-03-30 09:01:06 -0400

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Chris Feher on 2020-03-30.
+//  Copyright © 2020 Chris Feher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic Tac ToeTests/Tic_Tac_ToeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tic_Tac_ToeTests.swift
+//  Tic Tac ToeTests
+//
+//  Created by Chris Feher on 2020-03-30.
+//  Copyright © 2020 Chris Feher. All rights reserved.
+//
+
+import XCTest
+@testable import Tic_Tac_Toe
+
+class Tic_Tac_ToeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
